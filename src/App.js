9fd3dcc514f9f37c0142d641848a6715a702868c
Shantanu Kapse <shantanu.sk05@gmail.com>
2024-04-02 17:35:55 +0530

    basic frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$600<span>.00</span></h1>
+
+      {/* <h4>{dat.date}</h4> */}
+      <form>
+        <div className="info">
+          <input type="text" placeholder={"Price"}/>
+          <input type="datetime-local" placeholder={"tdays time and date by fetch"}/>
+        </div>
+        <div className="description">
+          <input type="text" placeholder="Description"/>
+        </div>
+        <button type="submit"> Add new transaction</button>
+      </form>
+
+{/*--------------------------------------------------------- */}
+      <div className="transactions">
+        <div className="trans">
+          <div className="left">
+            <div className="name">Samsung TV</div>
+            <div className="descript">56 inch ka tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-56000Rs</div>
+            <div className="timedate">20-09-23 12:23</div>
+          </div>
+        </div>
+    
+        <div className="trans">
+          <div className="left">
+            <div className="name">Job salary</div>
+            <div className="descript">Advanced</div>
+          </div>
+          <div className="right">
+            <div className="price green">+40000Rs</div>
+            <div className="timedate">20-09-23 12:23</div>
+          </div>
+        </div>
+     
+        <div className="trans">
+          <div className="left">
+            <div className="name">Nokia Phone</div>
+            <div className="descript">phonee</div>
+          </div>
+          <div className="right">
+            <div className="price red">-26000Rs</div>
+            <div className="timedate">20-09-23 12:23</div>
+          </div>
+        </div>
+      </div>
+      
+
+    </main>
   );
 }
 
